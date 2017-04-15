eb15c9798dcd59f377185e738d38635ba1d3be69
Howon Lee <howon@howonlee.com>
2017-04-15 15:58:11 +0800

    some things on warningprop basically

--- warningprop.py
+++ warningprop.py
@@ -1,8 +1,16 @@
 from read_cnf import read_cnf
 import sys
-import random
+import numpy as np
+import numpy.random as npr
 
 def initialize_warnings(clauses):
+    """ warnings as buncha numpy arrs? """
+    warnings = []
+    for clause in clauses:
+        warnings.append(npr.randint(2, size=len(clause)))
+    return warnings
+
+def check_convergence(warnings):
     pass
 
 def warning_update(clauses, variables, warnings):
@@ -12,8 +20,12 @@ def warning_propagation(clauses,
                         variables,
                         n_iters=20000,
                         epsilon=0.001):
+    warnings = initialize_warnings(clauses)
     for curr_iter in xrange(n_iters):
-        pass
+        warnings = warning_update(clauses, variables, warnings)
+        if check_convergence(warnings):
+            return warnings
+    return False
 
 def clauses_to_variables(clauses, num_vars):
     # 0 will be empty
@@ -29,5 +41,5 @@ def clauses_to_variables(clauses, num_vars):
 if __name__ == "__main__":
     assert len(sys.argv) == 2
     clauses, num_vars, num_clauses = read_cnf(sys.argv[1])
-    clauses_to_variables(clauses, num_vars)
-    # warning_propagation(clauses)
+    variables = clauses_to_variables(clauses, num_vars)
+    warning_propagation(clauses, variables)
