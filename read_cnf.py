6c3e8beda1f9270c400caa7015bc9230fa1a52ba
Howon Lee <howon@howonlee.com>
2017-04-15 15:18:15 +0800

    rudimentary cnf reader will do

--- read_cnf.py
+++ read_cnf.py
@@ -5,9 +5,14 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     with open(filename, "r") as cnf_file:
         lines = cnf_file.readlines()
-        init_line, clauses = lines[0], lines[1:]
-    print init_line
-    print "============="
-    print "============="
-    print "============="
+        lines = [line for line in lines if line[0] != 'c']
+        first_line, clause_lines = lines[0], lines[1:]
+    _, _, num_vars, num_clauses = first_line.split()
+    num_vars, num_clauses = int(num_vars), int(num_clauses)
+    assert len(clause_lines) == num_clauses
+    clauses = []
+    for clause_line in clause_lines:
+        nums = map(int, clause_line.split())
+        # last member of nums is always 0
+        clauses.append(nums[:-1])
     print clauses
