087f720b490e1a8a690b5982634dd257a0719595
Howon Lee <howon@howonlee.com>
2017-04-15 16:15:37 +0800

    looking at warning prop itself, and good times with that one

--- warningprop.py
+++ warningprop.py
@@ -1,5 +1,6 @@
 from read_cnf import read_cnf
 import sys
+import copy
 import numpy as np
 import numpy.random as npr
 
@@ -10,21 +11,39 @@ def initialize_warnings(clauses):
         warnings.append(npr.randint(2, size=len(clause)))
     return warnings
 
-def check_convergence(warnings):
-    pass
+def check_convergence(new_warnings, warnings):
+    for warning_idx, warning in enumerate(warnings):
+        if not np.allclose(warning, new_warnings[warning_idx]):
+            return False
+    return True
 
 def warning_update(clauses, variables, warnings):
-    pass
+    for warning_idx in xrange(len(warnings)):
+        warnings[warning_idx] = npr.randint(2, size=len(warnings[warning_idx]))
+    return warnings
+##########################
+##########################
+##########################
+##########################
 
 def warning_propagation(clauses,
                         variables,
-                        n_iters=20000,
-                        epsilon=0.001):
-    warnings = initialize_warnings(clauses)
+                        n_iters=2000):
+    curr_warnings_1 = initialize_warnings(clauses)
+    curr_warnings_2 = copy.deepcopy(curr_warnings_1)
+    print "beginning warningprop..."
     for curr_iter in xrange(n_iters):
-        warnings = warning_update(clauses, variables, warnings)
-        if check_convergence(warnings):
-            return warnings
+        if curr_iter % 100 == 0:
+            print curr_iter, " / ", n_iters
+        curr_warnings_1 = warning_update(clauses, variables, curr_warnings_1)
+        if id(curr_warnings_1) == id(curr_warnings_2):
+            print "============ ACK ============="
+        if check_convergence(curr_warnings_1, curr_warnings_2):
+            return curr_warnings_1
+        # just exchange ref vars, to avoid copying too much
+        temp = curr_warnings_1
+        curr_warnings_1 = curr_warnings_2
+        curr_warnings_2 = temp
     return False
 
 def clauses_to_variables(clauses, num_vars):
