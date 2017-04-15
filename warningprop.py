7193dad8a0cf64887eb61e4723e3b3f1313eef71
Howon Lee <howon@howonlee.com>
2017-04-15 15:47:23 +0800

    maybe this will work?

--- warningprop.py
+++ warningprop.py
@@ -1,17 +1,33 @@
 from read_cnf import read_cnf
 import sys
+import random
 
-def warning_update():
+def initialize_warnings(clauses):
     pass
 
-def warning_propagation(clauses, n_iters=20000):
+def warning_update(clauses, variables, warnings):
     pass
+
+def warning_propagation(clauses,
+                        variables,
+                        n_iters=20000,
+                        epsilon=0.001):
+    for curr_iter in xrange(n_iters):
+        pass
 
 def clauses_to_variables(clauses, num_vars):
-    variables = [[] for _ in xrange(num_vars)]
+    # 0 will be empty
+    variables = [[] for _ in xrange(num_vars+1)]
+    for clause_idx, clause in enumerate(clauses):
+        for member in clause:
+            if member > 0:
+                variables[member].append(clause_idx)
+            else:
+                variables[-member].append(-clause_idx)
     return variables
 
 if __name__ == "__main__":
     assert len(sys.argv) == 2
     clauses, num_vars, num_clauses = read_cnf(sys.argv[1])
-    warning_propagation(clauses)
+    clauses_to_variables(clauses, num_vars)
+    # warning_propagation(clauses)
