2cd9bf7db9b3f28c7943b87e1dcfb85ccf87f6ad
Howon Lee <howon@howonlee.com>
2017-05-26 20:10:14 +0900

    ok, read an implementation

---
+++ surveyprop.py
@@ -0,0 +1,12 @@
+from read_cnf import read_cnf
+import sys
+import copy
+import numpy as np
+import numpy.random as npr
+
+def generate_random_instance(num_clauses, num_variables, k):
+    return variables, clauses
+
+if __name__ == "__main__":
+    num_clauses, num_variables, k = 4200, 1000, 3
+    generate_random_instance(num_clauses, num_variables, k)
